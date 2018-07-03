5196286511c1395c1635d5c90dde72523ae9a9ca
sladelu <704041404@qq.com>
2018-07-03 17:57:35 +0800

    nn

---
+++ catkin_ws/build/Onboard-SDK-ROS/dji_sdk/CMakeFiles/dji_sdk_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,43 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dji_sdk_generate_messages_lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/msg/MissionHotpointTask.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/msg/Waypoint.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/msg/MobileData.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/msg/MissionWaypointAction.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/msg/WaypointList.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/msg/Gimbal.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/msg/MissionWaypointTask.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/msg/MissionWaypoint.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MissionWpSetSpeed.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MissionStatus.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MissionHpUpload.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MissionHpUpdateYawRate.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MissionHpGetInfo.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/SetLocalPosRef.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MissionWpAction.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/SetHardSync.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/StereoVGASubscription.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/SendMobileData.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MissionWpUpload.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/DroneTaskControl.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/SetupCameraStream.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MissionHpResetYaw.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/CameraAction.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MFIOSetValue.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/QueryDroneVersion.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/Activation.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MissionWpGetSpeed.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MFIOConfig.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MissionHpUpdateRadius.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MissionHpAction.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/MissionWpGetInfo.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/StereoDepthSubscription.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/Stereo240pSubscription.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/SDKControlAuthority.lisp"
+  "/home/wsn/catkin_ws/devel/share/common-lisp/ros/dji_sdk/srv/DroneArmControl.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/dji_sdk_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
